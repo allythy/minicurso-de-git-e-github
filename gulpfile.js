221631e7bd116fde9aa36d32e66a6d7b97d3dc1b
allythy <allythyrenan@gmail.com>
2018-02-27 00:54:18 -0300

    alterando o css e removendo imagem

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,6 @@ var minifycss = require( 'gulp-minify-css' );
 var uglify = require('gulp-uglify');
 var concat = require( 'gulp-concat' );
 var rename = require( 'gulp-rename' );
-var imagemin = require('gulp-imagemin');
-var newer = require('gulp-newer');
-
 
 
 gulp.task('copy:pasta',function() {
@@ -28,17 +25,9 @@ gulp.task('js', function() {
       .pipe(gulp.dest('./assets/js'));
 });
 
-gulp.task('compress-img', function() {
-   return gulp.src('./vendor/img/*.{jpg,jpeg,png}')
-   .pipe(newer('./vendor/img/*.{jpg,jpeg,png}'))
-	.pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-	.pipe(gulp.dest('./assets/img/'));
-});
-
 gulp.task( 'watch', function() {
   gulp.watch( './assets/css/*.css', [ 'css' ] );
   gulp.watch( './assets/js/*.js', [ 'js' ] );
-  gulp.watch( './vendor/img/*.{jpg,jpeg,png}', ['compress-img']);
 });
 
-gulp.task('default', ['watch' , 'css', 'copy:pasta', 'js', 'compress-img']);
+gulp.task('default', ['watch' , 'css', 'copy:pasta', 'js']);
