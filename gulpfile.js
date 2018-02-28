dee8df80b4736781442ee8c80ceec4243233a730
allythy <allythyrenan@gmail.com>
2018-02-28 15:24:38 -0300

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,14 @@
 var gulp = require( 'gulp' );
-var minifycss = require( 'gulp-minify-css' );
-var concat = require( 'gulp-concat' );
-var rename = require( 'gulp-rename' );
+var sass = require('gulp-sass');
 
-gulp.task('copy:pasta',function() {
-   gulp.src('node_modules/materialize-css/dist/**/*')
-     .pipe(gulp.dest('vendor'));
-});
-
-gulp.task( 'css', function() {
-  gulp.src( './assets/css/*.css' )
-    .pipe(concat('concat.js'))
-    .pipe(rename('main.min.css'))
-    .pipe( minifycss() )
-    .pipe( gulp.dest( './assets/css/' ) );
+gulp.task('sass', function () {
+    return gulp.src('assets/css/sass/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task( 'watch', function() {
-  gulp.watch( './assets/css/*.css', [ 'css' ] );
+   gulp.watch( './assets/css/**/*.sass', [ 'sass' ] );
 });
 
-gulp.task('default', ['watch' , 'css', 'copy:pasta']);
+gulp.task('default', ['watch' , 'sass']);
