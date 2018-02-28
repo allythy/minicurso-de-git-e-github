8a0019808403c26606dc4f267b3be75fd79076dd
allythy <allythyrenan@gmail.com>
2018-02-28 12:25:55 -0300

    teste

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require( 'gulp' );
 var minifycss = require( 'gulp-minify-css' );
-var uglify = require('gulp-uglify');
 var concat = require( 'gulp-concat' );
 var rename = require( 'gulp-rename' );
-
 
 gulp.task('copy:pasta',function() {
    gulp.src('node_modules/materialize-css/dist/**/*')
@@ -18,16 +16,8 @@ gulp.task( 'css', function() {
     .pipe( gulp.dest( './assets/css/' ) );
 });
 
-gulp.task('js', function() {
-  return gulp.src( './assets/js/*.js')
-      .pipe(concat('app.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('./assets/js'));
-});
-
 gulp.task( 'watch', function() {
   gulp.watch( './assets/css/*.css', [ 'css' ] );
-  gulp.watch( './assets/js/*.js', [ 'js' ] );
 });
 
-gulp.task('default', ['watch' , 'css', 'copy:pasta', 'js']);
+gulp.task('default', ['watch' , 'css', 'copy:pasta']);
